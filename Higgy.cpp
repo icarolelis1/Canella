7b7b733dad0dbcd90e0280d7c89d8ea4216ceb0c
Icaro Lélis <icaro.lelis@fsstudio.com>
2022-03-28 12:31:49 -0300

    .dayone

---
+++ Higgy.cpp
@@ -0,0 +1,13 @@
+﻿// Higgy.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "Higgy.h"
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Higgy.h
@@ -0,0 +1,8 @@
+﻿// Higgy.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
